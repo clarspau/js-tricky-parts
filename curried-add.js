d6aa6c1e3c4bcd5c33547947cef7d0c1ac5f21c4
clarspau <125636195+clarspau@users.noreply.github.com>
2024-04-22 20:05:45 -0700

    curried-add.js

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,18 @@
 function curriedAdd(total) {
+     // if there's no initial value, return 0
+     if (total === undefined) {
+          return 0;
+     }
 
+     return function addNum(num) {
+          if (num === undefined) {
+               return total;
+          }
+
+          total = total + num;
+          // return the function so we can keep adding numbers
+          return addNum;
+     };
 }
 
 module.exports = { curriedAdd };
