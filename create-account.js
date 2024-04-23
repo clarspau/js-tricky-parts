2c045be91e99b22cc0c6e4b549410303dc19b764
clarspau <125636195+clarspau@users.noreply.github.com>
2024-04-22 19:20:32 -0700

    create-account.js

--- create-account.js
+++ create-account.js
@@ -1,5 +1,39 @@
-function createAccount(pin, amount) {
+function createAccount(pin, amount = 0) {
+     return {
+          checkBalance(inputPin) {
+               if (inputPin !== pin) {
+                    return "Invalid PIN.";
+               }
+               return `$${amount}`;
+          },
 
+          deposit(inputPin, depositAmount) {
+               if (inputPin !== pin) {
+                    return "Invalid PIN.";
+               }
+               amount += depositAmount;
+               return `Succesfully deposited $${depositAmount}. Current balance: $${amount}.`;
+          },
+
+          withdraw(inputPin, withdrawAmount) {
+               if (inputPin !== pin) {
+                    return "Invalid PIN.";
+               }
+               if (withdrawAmount > amount) {
+                    return "Withdrawal amount exceeds account balance. Transaction cancelled."
+               }
+               amount -= withdrawAmount;
+               return `Succesfully withdrew $${withdrawAmount}. Current balance: $${amount}.`;
+          },
+
+          changePin(inputPin, newPin) {
+               if (inputPin !== pin) {
+                    return "Invalid PIN.";
+               }
+               pin = newPin;
+               return "PIN successfully changed!"
+          }
+     };
 }
 
 module.exports = { createAccount };
